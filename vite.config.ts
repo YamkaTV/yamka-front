e0a1329e69a14d1ea6e8beb5cd0c454f57d1084e
Fristaylo <andru_2004@mail.ru>
2025-05-05 12:52:43 +0400

    Fix favicon and files routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Настройка для Vite
 export default defineConfig({
   plugins: [react()],
-  base: './', // фронт доступен с корня
-  publicDir: '../template', // Указываем папку с HTML как public
+  base: '/',
+  publicDir: '../template',
   build: {
-    outDir: '../dist',     // куда будет собираться фронт
-    emptyOutDir: true,     // очищать dist перед сборкой
+    outDir: '../dist',
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
-      // Алиас для компонентов
       '@components': '/src/components',
-      // Алиас для изображений, например, для использования в компонентах
-      '@images': '/public/images',  // Путь к папке с изображениями
+      '@images': '/public/images',
     },
   },
 });
